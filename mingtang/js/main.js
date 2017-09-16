47e0ca8ecd45d02a75936931a99b0bcdb3b22b92
jixingyu <ymx2011@gmail.com>
2017-09-16 20:04:58 +0800

    update

--- mingtang/js/main.js
+++ mingtang/js/main.js
@@ -1,3 +1,7 @@
 (function(){
-    
+    $(document).on("click",".dropdown-toggle",function(){
+        if( $(window).width() > 767 ){
+            if($(this).attr('href')) window.location = $(this).attr('href');
+        }
+    });
 })()
