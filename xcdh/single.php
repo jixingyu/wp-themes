150c77974367752ef70be6989026f12a58172509
jixingyu <ymx2011@gmail.com>
2018-10-29 09:33:26 +0800

    xcdh

--- xcdh/single.php
+++ xcdh/single.php
@@ -1,4 +1,11 @@
-<?php get_header(); ?>
+<?php get_header();
+  $xyCat = get_the_category();
+  if (!empty($xyCat)) {
+    $xyCatName = $xyCat[0]->name;
+  } else {
+    $xyCatName = '';
+  }
+?>
 
 <div class="news_Container">
   <div class="newscon pageW">
@@ -7,10 +14,12 @@
           <div class="newslist fl">
             <?php while( have_posts() ): the_post(); ?>
               <div class="newscontent">
+                <?php if ($xyCatName == '行业资讯') : ?>
                 <h2 class="title"><?php the_title();?></h2>
                 <p class="c">
                     <span class="time">日期：<?php the_time('Y-m-d H:i');?></span>
                 </p>
+                <?php endif; ?>
                 <div class="newscc">
                   <?php the_content(); ?>
                 </div>
